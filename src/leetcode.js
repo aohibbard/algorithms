f7a89089a91b83e7110a4160eb7ea8c4f74d223d
Andrew Hibbard <andrew.o.hibbard@gmail.com>
2020-08-19 17:21:27 +0100

    goat latin algorithm added

--- src/leetcode.js
+++ src/leetcode.js
@@ -1,3 +1,48 @@
+/*
+19 August - Goat Latin
+A sentence S is given, composed of words separated by spaces. Each word consists of lowercase and uppercase letters only.
+
+We would like to convert the sentence to "Goat Latin" (a made-up language similar to Pig Latin.)
+
+The rules of Goat Latin are as follows:
+
+If a word begins with a vowel (a, e, i, o, or u), append "ma" to the end of the word.
+For example, the word 'apple' becomes 'applema'.
+ 
+If a word begins with a consonant (i.e. not a vowel), remove the first letter and append it to the end, then add "ma".
+For example, the word "goat" becomes "oatgma".
+ 
+Add one letter 'a' to the end of each word per its word index in the sentence, starting with 1.
+For example, the first word gets "a" added to the end, the second word gets "aa" added to the end and so on.
+Return the final sentence representing the conversion from S to Goat Latin. 
+
+*/
+
+var toGoatLatin = function(S) {
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+  let sentence = S.split(' ')
+  let str = ''
+  for(let i=0; i< sentence.length; i++){
+      let word = sentence[i]
+      let counter = 0
+      let aCount = 'maa'
+      while(counter < i){
+          aCount += 'a'
+          counter++
+      }
+      if (vowels.includes(word[0])){
+          str += word + aCount
+      } else {
+          let newWord = word.slice(1, word.length) + word[0] + aCount
+          str += newWord
+      }
+      if ( i < sentence.length - 1){
+          str += ' '
+      }
+  }
+  return str
+};
+
 // PROBLEM 283 - MOVE ZEROES
 // Runtime: 84 ms, faster than 59.63% of JavaScript online submissions for Move Zeroes.
 // Memory Usage: 38.1 MB, less than 55.00% of JavaScript online submissions for Move Zeroes.
