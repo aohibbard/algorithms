b6ee54c727d7f2a0a3bbd12e88f81beba82fc912
Andrew Hibbard <andrew.o.hibbard@gmail.com>
2020-10-26 10:42:55 +0000

    champagne tower

--- src/leetcode.js
+++ src/leetcode.js
@@ -1,3 +1,18 @@
+// 799 champagne tower
+var champagneTower = function(poured, query_row, query_glass) {
+  let cups = [poured];
+  for(let i=0; i<query_row; i++){
+      let nextRow = new Array(cups.length+1).fill(0);
+      for(let j=0; j<cups.length; j++){
+          let extraOverFlow = Math.max(0,cups[j]-1);
+          nextRow[j] += extraOverFlow/2;
+          nextRow[j+1] += extraOverFlow/2;
+      }
+      cups = nextRow;
+  }
+  return Math.min(1,cups[query_glass]);
+};
+
 // 456 132 pattern
 var find132pattern = function(nums) {
   let stack = []
