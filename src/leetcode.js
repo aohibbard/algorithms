cea26ad24165fc76bdda1366aaa32e7de48ab8be
Andrew Hibbard <andrew.o.hibbard@gmail.com>
2020-10-26 10:34:08 +0000

    new algo

--- src/leetcode.js
+++ src/leetcode.js
@@ -1,3 +1,17 @@
+// 456 132 pattern
+var find132pattern = function(nums) {
+  let stack = []
+  let s3 = -Infinity
+  for (let i = nums.length - 1; i >= 0; i--) {
+      if (nums[i] < s3) return true
+      while (stack.length > 0 && stack[stack.length - 1] < nums[i]) {
+          s3 = stack.pop()
+      }
+      stack.push(nums[i])
+  }
+  return false
+};
+
 // 414 third max
 // var thirdMax = function(nums) {
 //     nums = [...new Set(nums)]
