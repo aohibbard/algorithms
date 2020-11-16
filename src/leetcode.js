3a21d46053f8aa15e4004947543d475a0ee978ce
Andrew Hibbard <andrew.o.hibbard@gmail.com>
2020-11-16 23:15:33 +0000

    in python

--- src/leetcode.js
+++ src/leetcode.js
@@ -19,6 +19,44 @@ MyStack.prototype.push = function(x) {
 MyStack.prototype.pop = function() {
   return this.stack.pop()
 };
+/*
+in Python
+class MyStack:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.stack = []
+        
+
+    def push(self, x: int) -> None:
+        """
+        Push element x onto stack.
+        """
+        self.stack.append(x)
+        
+
+    def pop(self) -> int:
+        """
+        Removes the element on top of the stack and returns that element.
+        """
+        n = self.stack.pop()
+        return n
+
+    def top(self) -> int:
+        """
+        Get the top element.
+        """
+        return self.stack[-1]
+        
+
+    def empty(self) -> bool:
+        """
+        Returns whether the stack is empty.
+        """
+        return len(self.stack) == 0
+*/
 
 /**
 * Get the top element.
