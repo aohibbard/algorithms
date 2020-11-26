35222dd236425d8b1cb699cd879f51cf09fc6d0a
Andrew Hibbard <andrew.o.hibbard@gmail.com>
2020-11-26 16:41:05 +0000

    string to integers

--- leetcode.py
+++ leetcode.py
@@ -35,3 +35,12 @@ class Solution:
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         return sorted(s) == sorted(t)
+
+# 1318 string to 2 integers
+class Solution:
+    def getNoZeroIntegers(self, n: int) -> List[int]:
+        i = 1
+        while '0' in str(n - i) or '0' in str(i):
+            i += 1
+        return [i, n - i]
+        
