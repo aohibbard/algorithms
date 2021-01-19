0281cd7de37c9d59798e5a7c710bac2ee73ce223
Andrew Hibbard <andrew.o.hibbard@gmail.com>
2021-01-19 12:59:09 +0000

    jan 19 daily challenge

--- leetcode.py
+++ leetcode.py
@@ -56,3 +56,36 @@ class Solution:
             if count == k:
                 return i
         return (n if count+1==k else -1)
+
+# jan 19 / number 5
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if len(s) == 1:
+            return s
+        res = ""
+        longest = ""
+        max_count = 0
+        
+        for i in range(len(s)):
+            res = ""
+            l, r = i, i+1
+            while 0 <= l and r < len(s) and s[l] == s[r]:
+                res = s[l] + res + s[r]
+                l -= 1
+                r += 1
+            if len(res) > max_count:
+                max_count = len(res)
+                longest = res
+        
+        for i in range(len(s)):
+            res = s[i]
+            l, r = i - 1, i + 1
+            while 0 <= l and r < len(s) and s[l] == s[r]:
+                res = s[l] + res + s[r]
+                l -= 1
+                r += 1
+            if len(res) > max_count:
+                max_count = len(res)
+                longest = res
+        
+        return longest
